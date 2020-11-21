9527df465dcb269f93f0df7c93680e981e8911af
printbart <jklee4365@gmail.com>
2020-11-21 15:45:56 -0500

    Initial Commit

---
+++ timecapsule/timecapsuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  timecapsuleApp.swift
+//  timecapsule
+//
+//  Created by James Lee on 2020-11-21.
+//
+
+import SwiftUI
+
+@main
+struct timecapsuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
